06695be84b0b2c0e20ecda2c425ed812117f2e0a
Jeff-Mardoqueo <jeffreymardoqueo260@gmail.com>
2025-06-30 12:24:07 -0600

    Create Interface of Login User

---
+++ SchoolFees.Application/users/Repositories/IUserLoginRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SchoolFees.Application.users.DTOs;
+
+namespace SchoolFees.Application.users.Repositories
+{
+    public interface IUserLoginRepository
+    {
+        public Task<LoginRequest> UserLoginAsyn(LoginRequest loginRequest);
+        public Task<bool> UserLogoutAsync(string userId);
+    }
+}
