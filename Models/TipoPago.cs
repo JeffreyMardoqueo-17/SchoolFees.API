eadc1bba1eed6fab956d264c1406f454270d5ab9
Jeff-Mardoqueo <jeffreymardoqueo260@gmail.com>
2025-08-06 13:15:39 -0600

    Creation of the Class end Interfaces

--- Models/TipoPago.cs
+++ Models/TipoPago.cs
@@ -1,6 +1,12 @@
-﻿namespace SchoolFees.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolFees.API.Models
 {
     public class TipoPago
     {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "El maximo de caracteres es de 100")]
+        public string? Name { get; set; } = String.Empty();
     }
 }
---
+++ Services/TipoPago/ITipoPago.cs
@@ -0,0 +1,11 @@
+﻿using SchoolFees.API.Models;
+namespace SchoolFees.API.Services;
+
+public interface ITipoPago
+{
+    Task<IEnumerable<TipoPago>> GetAllTipoPagoAsinc();
+    Task<TipoPago> GetByIdTipoPagoAsync(int id);
+    Task CreateTipoPagoAsync(TipoPago tipoPago);
+    Task UpdateTipoPagoAsync(TipoPago tipoPago);
+    Task DeleteTipoPago(int id);
+}
