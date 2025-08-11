603f83e6f4e4256149f78bd78eec107d5e392011
Jeff-Mardoqueo <jeffreymardoqueo260@gmail.com>
2025-08-10 19:01:13 -0600

    Creacion del mapper de los Dtos de Tipo documentos

---
+++ DTOs/TipoDocumento/TipoDocCreate.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolFees.API.DTOs.TipoDocumento
+{
+    record class TipoDocCreate(
+        [property: Required(ErrorMessage = "El Nombre es requerido")]
+        [property: StringLength(100,ErrorMessage = "El maximo de caracteres permitidos es de 50")]
+        string Name
+        );
+}
---
+++ DTOs/TipoDocumento/TipoDocReadDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SchoolFees.API.DTOs.TipoDocumento
+{
+    public record TipoDocReadDto
+    (
+        int id,
+        string name 
+    );
+}
