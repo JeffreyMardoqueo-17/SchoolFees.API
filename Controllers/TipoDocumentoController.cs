fc73272291c7a07b0e00d78ff0ee9dfba8966e8f
Jeff-Mardoqueo <jeffreymardoqueo260@gmail.com>
2025-08-10 19:19:33 -0600

    creating of the controller

---
+++ Controllers/TipoDocumentoController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolFees.API.DTOs.TipoDocumento;
+using SchoolFees.API.DTOs.TipoPago;
+using SchoolFees.API.Services.TypeDocumento;
+
+namespace SchoolFees.API.Controllers
+{
+    public class TipoDocumentoController : ControllerBase
+    {
+        private readonly ITipoDocumento _tipoDocumentoService;
+        private readonly Mapper _mapper;
+        public TipoDocumentoController(ITipoDocumento tipoDocumento, Mapper mapper)
+        {
+            _tipoDocumentoService = tipoDocumento;
+            _mapper = mapper;
+        }
+        //GET: api/tipodocumento
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TipoDocReadDto>>> GetAllTipoDocumento()
+        {
+            var tipoDocumentos = await _tipoDocumentoService.GetAllTipoDocumentoAsinc();
+            var resul = _mapper.Map<IEnumerable<TipoDocReadDto>>(tipoDocumentos);
+            return Ok(resul);
+        }
+        //get : api/tipodocuemnto/{id}
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<TipoDocReadDto>> GetByIdTipoDocumento(int id)
+        {
+            var tipoDocumento = await _tipoDocumentoService.GetByIdTipoDocumento(id);
+            if (tipoDocumento == null)
+                NotFound(new { message = $"No se encontro ningun tipo de documento con este id: {id}" });
+            var result = _mapper.Map<TipoDocReadDto>(tipoDocumento);
+            return Ok(result);
+        }
+
+    }
+}
