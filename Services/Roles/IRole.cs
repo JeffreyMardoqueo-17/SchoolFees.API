1e909b5cbd2488b0f6c4b3016fe23ee93e9f6ff3
Jeff-Mardoqueo <jeffreymardoqueo260@gmail.com>
2025-08-24 11:30:31 -0600

    Creation of service of the class Role

---
+++ Services/Roles/IRole.cs
@@ -0,0 +1,10 @@
+﻿using SchoolFees.API.Models;
+
+namespace SchoolFees.API.Services.Roles
+{
+    public interface IRole
+    {
+        Task<List<Role>> GetAllRoleAsync();
+        Task<Role> GetRoleByIdAsync(int id);
+    }
+}
