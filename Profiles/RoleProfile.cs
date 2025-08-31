7944bd8b8f97703e96428fd562e3a1422a324ebd
Jeff-Mardoqueo <jeffreymardoqueo260@gmail.com>
2025-08-31 08:09:27 -0600

    Creacion del mapper de role

---
+++ Profiles/RoleProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SchoolFees.API.Models;
+using SchoolFees.API.DTOs.Change;
+using SchoolFees.API.DTOs.Roles;
+
+namespace SchoolFees.API.Profiles
+{
+    public class RoleProfile : Profile
+    {
+        public RoleProfile()
+        {
+            //lectura
+            CreateMap<Role, RoleReadDto>()
+                .ForCtorParam("Name", opt => opt.MapFrom(src => src.Name ?? string.Empty));
+        }
+    }
+}
